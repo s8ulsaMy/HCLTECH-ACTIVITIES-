5562f7efcc88692cb955abf20d3798ca96e386d5
s8ulsaMy <samsoloman76@gmail.com>
2026-02-17 09:56:11 +0530

    fixed inheritence file

--- Module_1/Inheritance_3.java
+++ Module_1/Inheritance_3.java
@@ -4,7 +4,8 @@ public class Inheritance_3 {
     int quantity;
     int lowOrderLevelQuantity;
 
-    public Inventory(int quantity, int lowOrderLevelQuantity) {
+    // Fixed: Constructor name now matches the class name
+    public Inheritance_3(int quantity, int lowOrderLevelQuantity) {
         this.quantity = quantity;
         this.lowOrderLevelQuantity = lowOrderLevelQuantity;
     }
