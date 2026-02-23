cb242270690accf9b492f2f148fc1746ed3195de
ShanuRajput12 <shanurajput2004@gmail.com>
2026-02-23 09:10:38 +0530

    new file

--- Module_5/chapter_1/activity_1/SimpleInterestPublisher.java
+++ Module_5/chapter_1/activity_1/SimpleInterestPublisher.java
@@ -7,8 +7,9 @@ public class SimpleInterestPublisher {
     public static void main(String[] args) {
 
         Endpoint.publish(
-                "http://localhost:8080/SimpleInterest",
-                new SimpleInterestImpl());
+            "http://localhost:8080/SimpleInterest",
+            new SimpleInterestImpl()
+        );
 
         System.out.println("Simple Interest Web Service is published!");
         System.out.println("WSDL available at:");
